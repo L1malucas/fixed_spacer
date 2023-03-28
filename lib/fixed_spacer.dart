69c91773ac3cdaaad6b06a3f8d833993f8e9859c
lucas lima <ljesus@reconcavo.org.br>
2023-03-28 19:33:53 -0300

    initial struct

---
+++ lib/fixed_spacer.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library fixed_spacer;
+
+export 'src/fixed_spacer_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
